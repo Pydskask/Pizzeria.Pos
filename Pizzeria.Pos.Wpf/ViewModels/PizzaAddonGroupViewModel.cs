dc20a8a90db1defa5face376c211f87dd56eb38d
Pydskask <119056659+Pydskask@users.noreply.github.com>
2026-03-28 00:42:31 +0100

    feat: PizzaAddonGroupViewModel - dodano IsSelected, SelectedCount, RefreshSelectedCount

--- Pizzeria.Pos.Wpf/ViewModels/PizzaAddonGroupViewModel.cs
+++ Pizzeria.Pos.Wpf/ViewModels/PizzaAddonGroupViewModel.cs
@@ -1,10 +1,24 @@
-﻿using System.Collections.ObjectModel;
+using CommunityToolkit.Mvvm.ComponentModel;
+using System.Collections.ObjectModel;
+using System.Linq;
 
 namespace Pizzeria.Pos.Wpf.ViewModels
 {
-    public class PizzaAddonGroupViewModel
+    public partial class PizzaAddonGroupViewModel : ObservableObject
     {
         public string GroupName { get; set; } = string.Empty;
+
         public ObservableCollection<PizzaAddonOptionViewModel> Items { get; } = new();
+
+        [ObservableProperty]
+        private bool isSelected;
+
+        [ObservableProperty]
+        private int selectedCount;
+
+        public void RefreshSelectedCount()
+        {
+            SelectedCount = Items.Count(x => x.IsSelected);
+        }
     }
 }
