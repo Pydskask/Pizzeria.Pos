44fb172e363543d785a98fcffae48a6fe7dc2841
Pydskask <olekolek123@gmail.com>
2026-03-28 00:45:48 +0100

    poprawki v3

--- Pizzeria.Pos.Wpf/obj/Debug/net8.0-windows7.0/Pizzeria.Pos.Wpf.AssemblyInfo.cs
+++ Pizzeria.Pos.Wpf/obj/Debug/net8.0-windows7.0/Pizzeria.Pos.Wpf.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Pizzeria.Pos.Wpf")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab245b0f15c6030364c131e2ab711cc1a972bb67")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc20a8a90db1defa5face376c211f87dd56eb38d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Pizzeria.Pos.Wpf")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Pizzeria.Pos.Wpf")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
